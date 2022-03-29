b514a04aa1c75f07869f884afb690fd86991e8ab
muhamadsafrudin <safrudinlaptop@gmail.com>
2022-03-29 22:27:12 +0700

    api

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -1,3 +1,4 @@
-const APimakanan = "http://localhost/server-makanan/api";
+// const APimakanan = "http://localhost/server-makanan/api";
+const APimakanan = "https://klikajaslur.000webhostapp.com/server-makanan/api";
 
 export default APimakanan;
