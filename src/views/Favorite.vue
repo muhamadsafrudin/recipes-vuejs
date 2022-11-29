afd3292280d9334f6cbd3fe4b475a45834b3283e
muhamadsafrudin <safrudinlaptop@gmail.com>
2022-11-29 22:57:06 +0700

    update

--- src/views/Favorite.vue
+++ src/views/Favorite.vue
@@ -30,7 +30,12 @@ import Footer from '../components/Footer.vue';
           <div class="col">
             <div class="card shadow-sm">
               <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-              <img class="bd-placeholder-img card-img-top" v-bind:src="recipe.thumb">
+              <img class="bd-placeholder-img card-img-top" v-bind:src="
+               recipe.thumb == 'null'
+                    ? '/assets/icon/noimage.jpeg'
+                    : recipe.thumb
+              ">
+     
               <div class="card-body">
                 <h5>{{ recipe.title }}</h5>
 
