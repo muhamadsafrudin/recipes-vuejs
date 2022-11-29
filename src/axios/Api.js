05bd4d546ca63fed79252acaed5c05681bb87357
muhamadsafrudin <safrudinlaptop@gmail.com>
2022-11-29 22:30:49 +0700

    update

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -2,7 +2,7 @@
 // const APimakanan = "https://klikajaslur.000webhostapp.com/server-makanan/api";
 // const APimakanan = "http://server-makanan.herokuapp.com/api";
 
-const APimakanan = "http://localhost:3030";
+const APimakanan = "https://server-makanan.herokuapp.com";
 
 const APiKey     = {
     "api-key" : "masak_apa"
