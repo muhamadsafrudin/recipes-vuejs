7b427e43d86fca31153faca6f777091d714e20bb
muhamadsafrudin <safrudinlaptop@gmail.com>
2022-12-13 22:55:00 +0700

    update

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,9 +1,21 @@
+
 <template>
     <div v-for="(n, index) in 5" :key="index">
       <div class="col">
         <div class="card shadow-sm">
-          <img class="bd-placeholder-img card-img-top" v-bind:src="'/assets/icon/load.gif'" />
+          <!-- <img class="bd-placeholder-img card-img-top" v-bind:src="'/assets/icon/load.gif'" /> -->
+          <Skeleton :width="'100%'" />
         </div>
       </div>
     </div>
 </template>
+
+<script>
+
+  import { Skeletor } from 'vue-skeletor';
+
+  export default {
+    components: { Skeletor }
+  }
+
+</script>
